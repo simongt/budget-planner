d75bcdf17ea6467d79266c7b709ce7f06ec4835e
Sy <simon.tsegay@gmail.com>
2020-04-21 14:07:53 -0400

    config firebase

---
+++ src/services/firebase.js
@@ -0,0 +1,32 @@
+import firebase from 'firebase';
+import 'firebase/auth';
+import 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyBkWPPjzQEgw3R1OsCfWL_m-Hi8HRFjZT4',
+  authDomain: 'project-a5ba5.firebaseapp.com',
+  databaseURL: 'https://project-a5ba5.firebaseio.com',
+  projectId: 'project-a5ba5',
+  storageBucket: 'project-a5ba5.appspot.com',
+  messagingSenderId: '386034505060',
+  appId: '1:386034505060:web:aaa7a6b974d718fdaad671',
+  measurementId: 'G-QH32PNQ2K1'
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth;
+export const db = firebase.database();
+
+export function signup(email, password) {
+  return auth().createUserWithEmailAndPassword(email, password);
+}
+
+export function signin(email, password) {
+  return auth().signInWithEmailAndPassword(email, password);
+}
+
+export function signInWithGoogle() {
+  const provider = new auth.GoogleAuthProvider();
+  return auth().signInWithPopup(provider);
+}
