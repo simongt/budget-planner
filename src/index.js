affdfaefc798229923800e4fcb0a169c05bcd12a
Sy <simon.tsegay@gmail.com>
2020-04-21 01:05:36 -0400

    tweak app entry point

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Auth, Home, Landing, Report, Slider } from './js/components';
+import { App } from './js/components';
 
 const wrapper = document.getElementById('root');
 
-wrapper ? ReactDOM.render(<Landing />, wrapper) : false;
+wrapper ? ReactDOM.render(<App />, wrapper) : false;
--- src/js/components/index.js
+++ src/js/components/index.js
@@ -1,7 +1,8 @@
+import App from './App';
 import Landing from './Landing';
 import Auth from './Auth';
 import Home from './Home';
 import Report from './Report';
 import Slider from './Slider';
 
-export { Auth, Home, Landing, Report, Slider };
+export { App, Auth, Home, Landing, Report, Slider };
